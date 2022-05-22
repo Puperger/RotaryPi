e5cc2e2fe52838121dbb470057c3b107836887ef
Puperger <82495364+Puperger@users.noreply.github.com>
2022-05-22 18:31:14 +0200

    Read rotary Values

--- Rotary.py
+++ Rotary.py
@@ -18,3 +18,28 @@ counter1=0
 counter2=0
 clkLastState1=GPIO.input(clk1)
 clkLastState2=GPIO.input(clk2)
+
+
+#Dictionairies for Converting from Rotaries to Class
+
+class = {0:1,2:2,4:3,6:4,8:5,10:6,12:"ALLE"}
+letters={0:"a",0:"a",2:"b",4:"c",6:"d",8:"e",10:"f",12:"g",14:"h",16:"i",18:"j",20:"k",22:"l",24:"m",26:"ALLE"}
+
+while True:
+    clkState1 = GPIO.input(clk1)
+    dtState1  = GPIO.input(dt1)
+    clkState2 = GPIO.input(clk2)
+    dtState2  = GPIO.input(dt2)
+    if clkState1 != clkLastState1:
+        counter1=(counter1+(2*clkState1!=dtState1)-1)%40
+    clkLastState1=clkState1
+
+    #Second Rotary Encoder
+    if clkState1 != clkLastState1:
+        counter1=(counter1+(2*clkState1!=dtState1)-1)%40
+    clkLastState1=clkState1
+
+
+
+    sleep(0.01)
+
